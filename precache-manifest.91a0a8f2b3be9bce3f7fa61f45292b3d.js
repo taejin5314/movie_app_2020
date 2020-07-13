429b6e2d84ef53727dcad3b6da59db4c487b86fc
taejin <taejin5314@gmail.com>
2020-07-13 19:35:18 -0400

    Updates

---
+++ precache-manifest.91a0a8f2b3be9bce3f7fa61f45292b3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6917b40e471e8be7c0b78256986c799a",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "6c293c5197e1d725defb",
+    "url": "/movie_app_2020/static/css/main.7d1b5267.chunk.css"
+  },
+  {
+    "revision": "b4ad6d5796d4c9c3f3c1",
+    "url": "/movie_app_2020/static/js/2.3507762e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.3507762e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c293c5197e1d725defb",
+    "url": "/movie_app_2020/static/js/main.d258651b.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
